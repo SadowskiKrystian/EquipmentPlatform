6296d487b4975d511949f64c3dae977b2f9127c4
SadowskiKrystian <ksprogramming@outlook.com>
2024-06-08 16:33:01 +0100

    add develop properties and integration test properties

--- src/main/java/com/ksprogramming/equipment/EquipmentApplication.java
+++ src/main/java/com/ksprogramming/equipment/EquipmentApplication.java
@@ -1,4 +1,4 @@
-package com.example.equipment;
+package com.ksprogramming.equipment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
